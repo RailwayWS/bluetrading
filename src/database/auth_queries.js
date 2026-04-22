da456eeee76aa82f12b90396c22edfe19d362f6f
Ruben Smith <27295451@sun.ac.za>
2026-04-22 12:08:06 +0200

    Added more auth changes

--- src/database/auth_queries.js
+++ src/database/auth_queries.js
@@ -1,8 +1,9 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
+
 export function sign_up(email, password) {
     const auth = getAuth();
-
+    
     createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
         const user = userCredential.user;
@@ -18,7 +19,7 @@ export function sign_up(email, password) {
 
 export function sign_in(email, password) {
     const auth = getAuth();
-
+    
     signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
         const user = userCredential.user;
